267cd192f478bc61cca92b5193e45c7fce3b7678
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2022-01-06 21:05:49 +0700

    fundamental2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,35 @@
-'use strict';
+// 'use strict';
 
-let hasCert=false;
-const passTest=true;
-if(passTest) {hasCert=true};
-if(hasCert) {console.log("You can drive");}
+// let hasCert=false;
+// const passTest=true;
+// if(passTest) {hasCert=true};
+// if(hasCert) {console.log("You can drive");}
+
+const info={
+    firstName:'Nairubi',
+    lastName:'Cẩm Tú',
+    birthYear:2000,
+    job:'teacher',
+    hasDriversLicense:false,
+
+    // calcAge:function () {
+    //     console.log(this);
+    //     return 2022-this.birthYear;
+    // }
+    calcAge:function () {
+        this.age=2022-this.birthYear;
+        return this.age;
+    },
+    // calcAge:function (birthYear) {
+    //     return 2022-birthYear;
+    // }
+
+    getSummary:function () {
+        return `${this.firstName} ${this.lastName} is a ${this.age} years old ${this.job} and she has ${this.hasDriversLicense? 'a':'no'} driver's license`
+    }
+}
+console.log(info.calcAge())
+// console.log(info.calcAge(2000))
+
+console.log(info.getSummary())
+
