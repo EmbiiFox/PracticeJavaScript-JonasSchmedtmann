9ff0235560e8889b38367db46128e4e524530e0a
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2022-01-20 01:57:52 +0700

    Challenge #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -391,41 +391,119 @@ console.log(ordersSet.has('Bread'));
 console.log(ordersSet.add('Tomatoes'))
 // console.log(ordersSet.delete('Pizza'))
 // console.log(ordersSet.clear())
-console.log(ordersSet);
-for(const order of ordersSet) {
-  console.log(order);
+// console.log(ordersSet);
+// for(const order of ordersSet) {
+//   console.log(order);
+// }
+// //Example: 
+// const staff=['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// const staffUnique= [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size)
+
+
+// console.log('____________________________MAP_________________');
+// const rest =new Map();
+// rest.set('name','Nairubi cẩm tú');
+// rest.set(1,'Vietnam, Thailand');
+// console.log(rest.set(2,'China, Korea'));
+// const test=rest
+//   .set('categories',['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open',11).set('close',23)
+//   .set(true,'OPEN')
+//   .set(false,'CLOSE');
+// console.log(test);
+// console.log(rest.get(true))
+
+
+// const time=21;
+// console.log( rest.get(time>rest.get('open')&&time<rest.get('close')?false:true));
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// console.log(rest);
+// console.log(rest.size);
+
+// const arr=[1,2];
+// rest.set(arr,"Test");
+// rest.set(document.querySelector('h1'),'Heading')
+// console.log(rest);
+// console.log(rest.size);
+// console.log(rest.get(arr));
+
+const question= new Map([
+  ['question',"Cô gái nào dễ thương hiền lành tốt bụng baby so ciu nhất quả đất?"],
+  [1,'Cô gái bàn bên'],
+  [2,'Cô gái nhà người ta'],
+  [3,'Hông có cô nào hết'],
+  [4,'Nairubi Cẩm Tú chứ còn ai vào đây nữa'],
+  ['correct',4],
+  [true,'Correct ❤'],
+  [false,"Try again!"]
+]);
+console.log(question);
+//convert object to map
+console.log(Object.entries(openingHours))
+const hoursMap= new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quiz application
+console.log(question.get('question'));
+for(const [key,value] of question){
+  if(typeof key==='number'){
+    console.log(`Answer ${key}: ${value}`);
+  }4
 }
-//Example: 
-const staff=['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-const staffUnique= [...new Set(staff)];
-console.log(staffUnique);
-console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size)
+// const answer=+prompt("Your answer is");
+const answer=4;
+console.log(answer);
+console.log(question.get(question.get('correct')===answer));
+//convert map to array
+console.log([...question])
+console.log('ENTRIES',[...question.entries()])
+console.log('KEY',[...question.keys()])
+console.log('VALUE',[...question.values()]);
 
+// Coding Challenge #3
 
-console.log('____________________________MAP_________________');
-const rest =new Map();
-rest.set('name','Nairubi cẩm tú');
-rest.set(1,'Vietnam, Thailand');
-console.log(rest.set(2,'China, Korea'));
-const test=rest
-  .set('categories',['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open',11).set('close',23)
-  .set(true,'OPEN')
-  .set(false,'CLOSE');
-console.log(test);
-console.log(rest.get(true))
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
 
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
 
-const time=21;
-console.log( rest.get(time>rest.get('open')&&time<rest.get('close')?false:true));
-console.log(rest.has('categories'));
-rest.delete(2);
-console.log(rest);
-console.log(rest.size);
+GOOD LUCK 😀
+*/
 
-const arr=[1,2];
-rest.set(arr,"Test");
-rest.set(document.querySelector('h1'),'Heading')
-console.log(rest);
-console.log(rest.size);
-console.log(rest.get(arr));
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+//1.
+// console.log(gameEvents.values())
+const events =[...new Set(gameEvents.values())] 
+console.log(events);
+//2.
+console.log(gameEvents.delete(64))
+console.log(gameEvents)
+//3.
+const time=[...gameEvents.keys()].pop();
+console.log(time);
+console.log(`"An event happened, on average, every ${time/gameEvents.size} minutes"`);
+//4.[FIRST HALF] 17: ⚽️ GOAL
+for(const [min,event] of gameEvents){
+  const half=min<=45?`[FIRST HALF]`:`[SECOND HALF]`
+  console.log(`${half} ${min}: ${event}`);
+
+}
