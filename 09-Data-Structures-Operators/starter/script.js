700d47289060618419be1785babc394a39ac47d4
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2022-01-19 19:34:44 +0700

    looping object

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,18 +4,8 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-  openingHours: {
+const openingHours={
     thu: {
       open: 12,
       close: 22,
@@ -28,7 +18,20 @@ const restaurant = {
       open: 0, // Open 24 hours
       close: 24,
     },
+  };
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours,
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
   orderDelivery: function ({
     starterIndex,
     mainIndex,
@@ -46,12 +49,12 @@ const restaurant = {
 
   }
 };
-restaurant.orderDelivery({
-  time: '22:30',
-  address: 'TMT17',
-  mainIndex: 2,
-  starterIndex: 1,
-});
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'TMT17',
+//   mainIndex: 2,
+//   starterIndex: 1,
+// });
 
 // const arr = [7, 9, 11];
 // const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
@@ -66,29 +69,29 @@ restaurant.orderDelivery({
 // restaurant.orderPasta(...ingredients);
 
 //Objects 
-const newRestaurant = {
-  ...restaurant,
-  founder: "embi",
-  foundedIn: 2023
-}
-console.log(newRestaurant)
+// const newRestaurant = {
+//   ...restaurant,
+//   founder: "embi",
+//   foundedIn: 2023
+// }
+// console.log(newRestaurant)
 
-const add = function (...numbers) {
-  // console.log(numbers)
-  let sum = 0
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  console.log(sum)
-}
-add(2, 3);
-add(2, 4, 4, 1, 5, 23, 5);
+// const add = function (...numbers) {
+//   // console.log(numbers)
+//   let sum = 0
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum)
+// }
+// add(2, 3);
+// add(2, 4, 4, 1, 5, 23, 5);
 
-const x = [23, 5, 7]
-add(...x);
+// const x = [23, 5, 7]
+// add(...x);
 
-restaurant.orderPizza('mushrooms', 'onion', 'peas');
-restaurant.orderPizza('mushrooms');
+// restaurant.orderPizza('mushrooms', 'onion', 'peas');
+// restaurant.orderPizza('mushrooms');
 
 // //DESTRUCTURING OBJECT
 // const {
@@ -183,15 +186,15 @@ restaurant.orderPizza('mushrooms');
 // restaurant.orderPizza && restaurant.orderPizza('mushroom', 'tomatoes');
 
 
-console.log("___________LOGICAL ASSIGNMENT OPERATORS_________________")
-const res1 = {
-  name: 'Nairbi',
-  numGuests: 0,
-}
-const res2 = {
-  name: 'Cam Tu',
-  owner: "Le Nguyen",
-}
+// console.log("___________LOGICAL ASSIGNMENT OPERATORS_________________")
+// const res1 = {
+//   name: 'Nairbi',
+//   numGuests: 0,
+// }
+// const res2 = {
+//   name: 'Cam Tu',
+//   owner: "Le Nguyen",
+// }
 // res1.numGuests = res1.numGuests || 10;
 // res2.numGuests = res2.numGuests || 10;
 //OR
@@ -208,8 +211,8 @@ const res2 = {
 // console.log(res2)
 
 
-
-//CHALLENGE 
+//////////////////////////////////////////////////////////////////
+//CHALLENGE#1
 
 const game = {
   team1: 'Bayern Munich',
@@ -268,23 +271,116 @@ const game = {
 
 // GOOD LUCK 😀
 //1.
-const [players1,players2] = game.players;
-console.log(players1,players2);
+// const [players1,players2] = game.players;
+// console.log(players1,players2);
 //2. 
-const [gk,...fieldPlayers]=players1;
-console.log(gk,fieldPlayers);
+// const [gk,...fieldPlayers]=players1;
+// console.log(gk,fieldPlayers);
 //3. 
-const fullPlayers=[...players1,...players2];
-console.log(fullPlayers)
+// const fullPlayers=[...players1,...players2];
+// console.log(fullPlayers)
 //4. 
-const players1Final=[...players1,'Thiago', 'Coutinho','Perisic'];
-console.log(players1Final)
+// const players1Final=[...players1,'Thiago', 'Coutinho','Perisic'];
+// console.log(players1Final)
 //5. 
-const {odds:{team1,x:draw,team2}}=game;
-console.log(team1,draw,team2);
+// const {odds:{team1,x:draw,team2}}=game;
+// console.log(team1,draw,team2);
 //6. 
-const printGoals=function(...players){
-  console.log(players)
-  console.log(`${players.length} goal were scored`)
+// const printGoals=function(...players){
+//   console.log(players)
+//   console.log(`${players.length} goal were scored`)
+// }
+// printGoals('Nairubi','embi','camtu');
+// printGoals(...game.scored);
+
+//7. 
+// const winner=team1>team2?console.log('Team 2 is more likely to win '):console.log('Team 1 is more likely to win ');
+// team1<team2&&console.log('Team 1 is more likely to win')
+// team1>team2&&console.log('Team 2 is more likely to win')
+
+
+
+// console.log("________________LOOPING ARRAY______________")
+// const menu=[...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+// for(const item of menu){
+//   console.log('const item of menu',item)
+// }
+// for(const [i,el] of menu.entries()){
+//   // console.log('entries()',i)
+//   console.log(`${i+1}:${el}`);
+// }
+// console.log(menu.entries());
+// console.log([...menu.entries()]);
+
+
+// console.log("___________LOOPING OBJECT________________")
+// console.log('Properties name')
+// const properties=Object.keys(openingHours)
+// console.log(properties)
+// let openStr=`We are open on ${properties.length} days: `;
+// for(const day of Object.keys(openingHours)){
+//   openStr+=`${day} `;
+// }
+// console.log(openStr)
+// console.log('Properties values')
+// const values=Object.values(openingHours);
+// console.log(values);
+
+//Entire object
+// const entries=Object.entries(openingHours);
+// console.log('Entries object', entries);
+//[key,value]
+// for(const [key, {open,close}] of entries) {
+//   console.log(`On ${key} we open at ${open} and close at ${close}`);
+// }
+
+/////////////////////////////////////////////////////
+console.log("__________CHALLENGE #2_____________");
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+//1. 
+// for(const player of game.scored){
+//   console.log(player)
+// }
+for(const [i,player] of game.scored.entries()) {
+  console.log(`Goal ${i}: ${player}`)
 }
-printGoals('Nairubi');
+//2. 
+const numbersValue=Object.values(game.odds);
+let average;
+let sum=0;
+for(const num of numbersValue){
+  console.log(num);
+  sum+=num;
+  average=sum/numbersValue.length;
+}
+console.log(`Average odds is ${average}`)
+//3. 
+for(const [team, odd] of Object.entries(game.odds)) {
+  console.log(team,odd)
+  const teamStr=team==='x'?'draw':`victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} :${odd}`)
+  // console.log(`Off of victory`)
+}
+
+console.log('__________________SET___________________')
