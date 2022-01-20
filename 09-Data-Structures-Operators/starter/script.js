83829f40967e39f6995dbb25785369b83fc4afc9
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2022-01-21 01:15:03 +0700

    string method practice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,10 +1,4 @@
 'use strict';
-
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-
 const openingHours={
     thu: {
       open: 12,
@@ -634,6 +628,7 @@ document.body.append(document.createElement('button'));
 document.querySelector('button').addEventListener('click', function () {
   const text = document.querySelector('textarea').value;
   const rows = text.split('\n');
+  console.log(typeof rows);
 
   for (const [i, row] of rows.entries()) {
     const [first, second] = row.toLowerCase().trim().split('_');
@@ -645,4 +640,20 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
-// console.log(eval(testArr));
+
+console.log('_________________________STRING METHOD PRACTICE___________________');
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+// console.log(flights.split('+'));
+const getCode=str=> str.slice(0,3).toUpperCase();
+for(const flight of flights.split('+')){
+  // console.log(flight.split(';'));
+  const [type,from,to,time]= flight.split(';');
+  // const output=`${type.startsWith('_Delayed')?'🔴':''} ${type.replaceAll('_',' ')} from ${from.toUpperCase().slice(0,3) } to ${to.toUpperCase().slice(0,3)} (${time.replace(':','h')})`;
+  const output=`${type.startsWith('_Delayed')?'🔴':''} ${type.replaceAll('_',' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':','h')})`;
+  console.log(output);
+}
