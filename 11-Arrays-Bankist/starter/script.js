5e8bd1afaf44ff3dabb34fa02b2d53197d89c164
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2022-01-24 23:57:00 +0700

    array

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,6 +71,51 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+//////////////////////////////////
+let arr=['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2));
+console.log(arr.slice(2,4));
+console.log(arr.slice(-2)); 
+console.log([...arr]);
+console.log(arr.splice(-1));
+console.log(arr.splice(1,3));
+
+//REVERSE
+console.log('REVERSE');
+arr=['a', 'b', 'c', 'd', 'e'];
+const arr2=['j','k','l','m','n'];
+console.log(arr2.reverse());
+
+console.log("CONCAT");
+const letters=arr.concat(arr2);
+console.log(letters);
+console.log([...arr,...arr2]);
+
+console.log('JOIN');
+console.log(letters.join('-'));
+
+console.log('THE NEW OF METHOD');
+const array=[23,11,64];
+console.log(array[0]);
+console.log(array.at(0));
+
+console.log('NORMAL FOR LOOP');
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+for(const [i,movement] of movements.entries()) {
+  if(movement>0){
+    console.log(`Movement ${i+1} You deposited ${movement}`);
+  }else{
+    console.log(`Movement ${i+1} You withdraw ${Math.abs(movement)}`);
+  }
+}
+console.log('FOREACH');
+movements.forEach(function(mov,i,arr){
+  if(mov>0){
+    console.log(`Movement ${i+1} You deposited ${mov}`);
+  }else{
+    console.log( `Movement ${i+1} You withdraw ${Math.abs(mov)}`);
+  }
+})
