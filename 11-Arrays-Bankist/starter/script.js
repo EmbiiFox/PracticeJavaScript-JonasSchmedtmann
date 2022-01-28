9f135730bc2312a77c7a87fc434f4795876d435e
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2022-01-28 23:33:39 +0700

    challenge #2

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -249,7 +249,13 @@ const calcAverageHumanAge=function(ages){
   const humanAge=ages.map(age=>age<=2?2*age:16+age*4);
   console.log(humanAge);
   const adults=humanAge.filter(age=>age>=18)
-  console.log(adults);
+  console.log('adults',adults);
+  // const average=adults.reduce((acc,age)=>age+acc)/adults.length;
+  const average=adults.reduce((acc,age,arr)=>age+acc/arr.length,0);
+  console.log('Average',average);
+  return average
 }
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
+const avg1= calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
+const avg2=calcAverageHumanAge([16,6,10,5,6,1,4]);
+console.log(avg1,avg2);
 
