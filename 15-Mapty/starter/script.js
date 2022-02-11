f40d42e97491c8b495454e7a695ea6edbe9a4200
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2022-02-11 23:00:33 +0700

    AJAX

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -13,6 +13,15 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
+class Workout{
+    date=new Date();
+    id=(new Date()+'').slice(-10)
+    constructor(coords, distance,duration){
+        this.coords=coords;
+        this.distance=distance;//in km 
+        this.duration=duration;//in min
+    }
+}
 class App{
     #map;
     #mapEvent;
